5c20e42658a66e80254af88c5147ba843c802a06
Ozan Güldali <ozan.guldali@hotmail.com>
2019-04-30 17:58:32 +0300

    fixed autowired requests bug

--- src/main/java/com/calculator/joker/controller/RequestController.java
+++ src/main/java/com/calculator/joker/controller/RequestController.java
@@ -2,6 +2,7 @@ package com.calculator.joker.controller;
 
 import com.calculator.joker.model.RequestModel;
 import com.calculator.joker.model.ResponseModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,12 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
 
-    private final
+    @Autowired
     JokerService jokerService;
-
-    public RequestController(JokerService jokerService) {
-        this.jokerService = jokerService;
-    }
 
     @RequestMapping(method = RequestMethod.POST, path = "/jokerDiscountCalculator")
     public ResponseModel response(@RequestBody RequestModel requestModel) {
