31c5db9dd7faeb4ee23fce670d64e55bf0f740e6
Ozan Güldali <ozan.guldali@hotmail.com>
2019-06-13 11:32:47 +0300

    added info alert text

---
+++ src/main/webapp/resources/js/closeInfoAlert.js
@@ -0,0 +1,17 @@
+let closeAlertButton = document.getElementById("close_alert");
+let i;
+
+closeAlertButton.onclick = function () {
+
+    let div = this.parentElement;
+    div.style.opacity = "0";
+
+    setTimeout(function() {
+
+        div.style.display = "none";
+
+        },
+
+    600);
+
+};
