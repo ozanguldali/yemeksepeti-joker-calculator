72053c3f535eacb612fe9d0ea1be26580baebd7e
Ozan Güldali <ozan.guldali@hotmail.com>
2019-08-02 15:13:34 +0300

    added alert auto closing

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -8,3 +8,16 @@ let table = document.getElementById('customer_list');
 let defaultDecimal = document.getElementById( 'decimal-0' );
 let cleanButton = document.getElementById('clear_button');
 let backButton = document.getElementById('back_button');
+
+window.setTimeout(function() {
+
+        $( '.alert' )
+            .fadeTo( 500, 0 )
+            .slideUp( 500, function() {
+
+                $( this ).remove();
+
+            });
+    }, 4000
+
+);
