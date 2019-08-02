c3dbbc26a266e34cdd4fb6fff604ccbe52ea8145
Ozan Güldali <ozan.guldali@hotmail.com>
2019-08-02 15:40:17 +0300

    added alert auto closing

--- src/main/webapp/resources/js/common.js
+++ src/main/webapp/resources/js/common.js
@@ -3,6 +3,7 @@ let inputForm = document.getElementById('input_form');
 let initialCostField = document.getElementById('initial_cost');
 let addButton = document.getElementById('add_button');
 let customerListDiv = document.getElementById( 'customerList' );
+// let submitButton = document.getElementById('submit_button');
 let table = document.getElementById('customer_list');
 let defaultDecimal = document.getElementById( 'decimal-0' );
 let cleanButton = document.getElementById('clear_button');
@@ -10,13 +11,13 @@ let backButton = document.getElementById('back_button');
 
 window.setTimeout(function() {
 
-        $( '.alert' )
-            .fadeTo( 500, 0 )
-            .slideUp( 500, function() {
+    $( '.alert' )
+        .fadeTo( 500, 0 )
+        .slideUp( 500, function() {
 
-                $( this ).remove();
+            $( this ).remove();
 
-            });
+        });
     }, 4000
 
 );
